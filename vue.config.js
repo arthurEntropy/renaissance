5a2ce3b74983c94016af3c7398a1e14eac737b59
arthurEntropy <ljbrevoort@gmail.com>
2025-02-13 20:40:43 -0500

    feat(001-character-sheet-conditions): Adds conditions column to character sheet.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    devtool: "eval-source-map", // You can try "eval-source-map" for faster rebuilding
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
+      })
+    ]
+  }
 })
