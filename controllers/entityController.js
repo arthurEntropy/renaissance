884103b51db4ba061366bbdaccf0c19fb0036525
arthurEntropy <ljbrevoort@gmail.com>
2025-04-09 11:17:59 -0400

    feat(028-controllers): Moves endpoints to separate controllers, implements dynamic endpoints for entities.

---
+++ controllers/entityController.js
@@ -0,0 +1,70 @@
+const { getDirectory, getAllDataByDirectory, saveFile, deleteFile } = require('../utils/fileService.js');
+
+const getAllEntities = (entity) => (req, res) => {
+  try {
+    const directory = getDirectory(entity);
+    const entities = getAllDataByDirectory(directory);
+    res.json(entities);
+  } catch (err) {
+    console.error(`Error reading ${entity} directory:`, err);
+    res.status(500).send(`Error reading ${entity} directory.`);
+  }
+};
+
+const createEntity = (entity) => (req, res) => {
+  const newEntity = req.body;
+
+  try {
+    const directory = getDirectory(entity);
+    saveFile(newEntity, directory);
+    res.status(201).send({ message: `${entity.slice(0, -1)} created successfully.`, id: newEntity.id });
+  } catch (error) {
+    console.error(`Error creating ${entity.slice(0, -1)}:`, error);
+    res.status(500).send(`Failed to create ${entity.slice(0, -1)}.`);
+  }
+};
+
+const updateEntity = (entity) => (req, res) => {
+  try {
+    const updatedEntity = req.body;
+    const directory = getDirectory(entity);
+    const entities = getAllDataByDirectory(directory);
+    const existingEntity = entities.find(e => e.id === updatedEntity.id);
+
+    if (existingEntity) {
+      saveFile(updatedEntity, directory, existingEntity.name);
+      return res.status(200).send(`${entity.slice(0, -1)} updated successfully.`);
+    } else {
+      return res.status(404).send(`No ${entity.slice(0, -1)} found to update.`);
+    }
+  } catch (error) {
+    console.error(`Error updating ${entity.slice(0, -1)}:`, error);
+    res.status(500).send(`Failed to update ${entity.slice(0, -1)}.`);
+  }
+};
+
+const deleteEntity = (entity) => (req, res) => {
+  try {
+    const entityId = req.params.id;
+    const directory = getDirectory(entity);
+    const entities = getAllDataByDirectory(directory);
+    const entityToDelete = entities.find(e => e.id === entityId);
+
+    if (entityToDelete) {
+      deleteFile(entityToDelete.name, directory);
+      return res.status(200).send(`${entity.slice(0, -1)} deleted successfully.`);
+    } else {
+      return res.status(404).send(`${entity.slice(0, -1)} not found.`);
+    }
+  } catch (error) {
+    console.error(`Error deleting ${entity.slice(0, -1)}:`, error);
+    res.status(500).send(`Failed to delete ${entity.slice(0, -1)}.`);
+  }
+};
+
+module.exports = {
+  getAllEntities,
+  createEntity,
+  updateEntity,
+  deleteEntity,
+};
