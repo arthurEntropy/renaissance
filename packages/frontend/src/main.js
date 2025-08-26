4eac467cd0974b7bd3a0186f3b9c7b3b751cff48
arthurEntropy <ljbrevoort@gmail.com>
2025-08-25 20:32:14 -0400

    feat(069): Various little tweaks

--- packages/frontend/src/main.js
+++ packages/frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 import '@/styles/design-tokens.css'
 import '@/styles/modals.css'
 import '@/styles/nav.css'
