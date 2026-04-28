7ed998f34df9823beed170765aa638e87714e882
arthurEntropy <ljbrevoort@gmail.com>
2026-04-28 14:28:39 -0400

    feat(130): Straggler prettier changes

--- packages/frontend/src/components/features/characterSheet/characterProfile/CharacterVitals.vue
+++ packages/frontend/src/components/features/characterSheet/characterProfile/CharacterVitals.vue
@@ -28,7 +28,7 @@
                     <span v-if="!cultures.length">None</span>
                     <span v-for="(culture, index) in cultures" :key="culture.id">
                         <router-link :to="`/cultures/${createSlug(culture.name)}`" class="concept-link">{{ culture.name
-                        }}</router-link><span v-if="index < cultures.length - 1">, </span>
+                            }}</router-link><span v-if="index < cultures.length - 1">, </span>
                     </span>
                 </div>
             </div>
